501617239d53798da78c1251bd0e3ab1a35da719
CriCMedina1992 <cri_calix@hotmail.com>
2016-11-23 00:15:26 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title> Constructora ACS</title>
+	
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12">
+		<h1>CONSTRUCTORA ACS</h1>
+
+
+
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
